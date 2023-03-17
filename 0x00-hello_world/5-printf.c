530f2759be720869b6bfe6bad2c8620c88b34519
Jambinno-pro <jambinnocreations@gmail.com>
2023-03-16 17:22:03 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the output is piece of art,\n");
+	return (0);
+}
