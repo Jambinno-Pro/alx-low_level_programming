bee4ec4ce545040bfb34ac50b99a89891c7c0b72
Jambinno-pro <jambinnocreations@gmail.com>
2023-03-22 07:42:28 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - make alphabet x10 times
- * Return: void
- */
-
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return: void
+  */
 void print_alphabet_x10(void)
 {
 	char c;
@@ -15,9 +15,11 @@ void print_alphabet_x10(void)
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-
-			_putchar('\n');
-			i++;
 		}
+
+		_putchar('\n');
+
+		i++;
 	}
 }
+
