59bba697f3481a2428636f4070bea707f295a128
Jambinno-Pro <jambinnocreations@gmail.com>
2023-03-23 16:12:28 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,14 @@
 
 /**
  * _isdigit - Check if a character is a digit
- * @c: the number to be checked
- * Return 1 for a character that'll be a digit or 0 for anything else
+ * @c: The number to be checked
+ * Return: 1 for a character that'll be a digit or 0 for any else
  */
 
 int _isdigit(int c)
+
 {
+
 	if (c >= 48 && c <= 57)
 	{
 	return (1);
