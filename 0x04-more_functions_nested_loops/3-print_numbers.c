1764f266c9e62c828002460d923ed7bd2368ebf7
Jambinno-Pro <jambinnocreations@gmail.com>
2023-03-24 08:34:19 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
 /**
- * print_numbers - Prints the numbers from 0-9
- */
-
+*print_numbers - print 0123456789
+*
+*Return: void
+*/
 void print_numbers(void)
 {
-	int n;
+	char c;
 
-	for (n = 0; n <= 9; n++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar((n % 10) + '0');
+	_putchar(c);
 	}
 	_putchar('\n');
+}
